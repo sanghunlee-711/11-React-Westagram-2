dde0c765707e72ca37aba3be0e63775a323750f8
sanghunLee <cloudlee711@gmail.com>
2020-08-12 09:51:28 +0900

    Modify: Login페이지 컴포넌트 분리 및 Scss 세부수정 완료

--- src/Pages/sanghunlee/Login/Login.js
+++ src/Pages/sanghunlee/Login/Login.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import LoginContent from './LoginContent'
-import LoginNav from './LoginNav'
+import LoginContent from './Components/LoginContent'
+import LoginNav from './Components/LoginNav'
 import './Login.scss';
-import './LoginContent.scss'
-import './LoginNav.scss'
+import './Components/LoginContent.scss'
+import './Components/LoginNav.scss'
 
 class Login extends React.Component {
     render(){
         return (
-            <div className ="Login">     
-                <section className="mainContentWrapper">
-                        <div className = "LoginContentwrapper">
+                <div className="Login">
+                        <section className = "LoginContentwrapper">
                             <img className="MainImage" alt = "phonePicture" src="../images/sanghunlee/leftiamge.png" />
                         <LoginContent />
-                        </div>
+                        </section>
                         <LoginNav />
-                </section>
-            </div>
+                </div>
             )
     }
 }
